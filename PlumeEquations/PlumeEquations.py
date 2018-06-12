b7fd5171dbbbf8777a5929cb91c0c2e27ecd42c9
Trista Hehnen <tristan.hehnen@cern.ch>
2018-06-12 14:57:48 +0200

    Adusted print statement

--- PlumeEquations/PlumeEquations.py
+++ PlumeEquations/PlumeEquations.py
@@ -6,16 +6,15 @@ Created on Mon Sep 05 12:09:20 2016
 """
 
 
-#%%
-
 '''
 Literature:
 [1] Enclosure Fire Dynamics, B. Karlsson, J. G. Quintiere, CRC Press LLC, 2000
 
+
+Note: In fire safety engineering the parameter heat release rate HRR is 
+frequently used. Within this document, energy release rate ERR is used instead. 
 '''
 
-
-#%%
 
 import numpy as np
 import csv
@@ -25,19 +24,20 @@ import re
 import glob
 import bisect
 
-#%%
 
 # Mean flame height
+#
 # Description:
-# L - Mean flame hight (in m)
+# L - Mean flame height (in m)
 # relD - relative diameter, assuming circular, planar fire source (in m)
 # ERR - Energy release rate (in kW)
 
+
 # Mean flame height
 # Formula 4.3 [1].
-def MeanFlameHeight(ERR,relD):
-    L = 0.235 *(ERR)**(2.0/5.0) - (1.02 * relD)
-    return L
+def mean_flame_height(err, rel_diam):
+    l = 0.235 * err ** (2.0 / 5.0) - (1.02 * rel_diam)
+    return l
  
 '''   
 # Test based on example 4.1 [1]. 
@@ -45,14 +45,13 @@ def MeanFlameHeight(ERR,relD):
 
 ERR = 1690.
 D = 1.6
-L = MeanFlameHeight(ERR,D)
+L = mean_flame_height(ERR,D)
 print 'Solution: ' + str(L) + ' m'
 '''
 
 
-#%%
-
 # Ideal (or weak) plume equations
+#
 # Description:
 # Based on following assumptions:
 # All energy released from point source, no radiation, density differences 
@@ -69,23 +68,25 @@ print 'Solution: ' + str(L) + ' m'
 
 # Upward gas velocity of the plume at height z
 # Formula 4.18 [1].
-def IdealPlumeVelocity(ERR,z,g=9.81,cp=1.0,T_infty=293,rho_infty=1.2):
-    a = 1.94*((g)/(cp * T_infty * rho_infty))**(1.0/3.0)
-    u = a *ERR**(1.0/3.0)*z**(-1.0/3.0)
+def ideal_plume_velocity(err, z, g=9.81, cp=1.0, T_infty=293, rho_infty=1.2):
+    a = 1.94*(g/(cp * T_infty * rho_infty))**(1.0/3.0)
+    u = a * err ** (1.0 / 3.0) * z ** (-1.0 / 3.0)
     return u
+
 
 # Mass flow of plume at height z
 # Formula 4.19 [1].
-def IdealPlumeMassFlow(ERR,z,g=9.81,cp=1.0,T_infty=293,rho_infty=1.2):
+def ideal_plume_mass_flow(err, z, g=9.81, cp=1.0, T_infty=293, rho_infty=1.2):
     a = 0.20 * ((g*rho_infty**2)/(cp*T_infty))**(1.0/3.0)
-    m = a *ERR**(1.0/3.0)*z**(5.0/3.0)
+    m = a * err ** (1.0 / 3.0) * z ** (5.0 / 3.0)
     return m
+
 
 # Temperature difference of the plume at height z
 # Formula 4.20 [1].
-def IdealPlumeTemperature(ERR,z,g=9.81,cp=1.0,T_infty=293,rho_infty=1.2):
-    a = 5.0*((T_infty)/(g * cp**2 * rho_infty**2))**(1.0/3.0)
-    T = a *ERR**(2.0/3.0)*z**(-5.0/3.0)
+def ideal_plume_temperature(err, z, g=9.81, cp=1.0, T_infty=293, rho_infty=1.2):
+    a = 5.0*(T_infty/(g * cp**2 * rho_infty**2))**(1.0/3.0)
+    T = a * err ** (2.0 / 3.0) * z ** (-5.0 / 3.0)
     return T
 
 
@@ -96,37 +97,33 @@ def IdealPlumeTemperature(ERR,z,g=9.81,cp=1.0,T_infty=293,rho_infty=1.2):
 
 ERR = 70
 z = 2
-m = IdealPlumeMassFlow(ERR,z)
-T = IdealPlumeTemperature(ERR,z)
-print 'Mass flow: ' + str(m) + ' kg/s'
-print 'Temperature: ' + str(T+293) + ' K'
+m = ideal_plume_mass_flow(ERR,z)
+T = ideal_plume_temperature(ERR,z)
+print('Mass flow: {} kg/s'.format(m))
+print('Temperature: {} K'.format(T+293))
 '''
 
 
-#%%
-
 # Zukoski plume equations
 # Description:
-# Based on the idael plume, with a small change of the factor 0.20 to 0.21,
+# Based on the ideal plume, with a small change of the factor 0.20 to 0.21,
 # after experimental measurements. Underestimates the mass flow a bit, but
 # overestimates it for smaller burners.
 
 # Mass flow of plume at height z
 # Formula 4.19 [1].
-def ZukoskiPlumeMassFlow(ERR,z,g=9.81,cp=1.0,T_infty=293,rho_infty=1.2):
+def zukoski_plume_mass_flow(err, z, g=9.81, cp=1.0, T_infty=293, rho_infty=1.2):
     a = 0.21 * ((g*rho_infty**2)/(cp*T_infty))**(1.0/3.0)
-    m = a *ERR**(1.0/3.0)*z**(5.0/3.0)
+    m = a * err ** (1.0 / 3.0) * z ** (5.0 / 3.0)
     return m
 
-
-#%%
 
 # Heskestad (or strong) plume equations
 # Description:
 # Based on ideal plume equations, modified by experimental data.
-# Assumptions: A virtual origin is introduced at hight z0, convective ERR Q0
+# Assumptions: A virtual origin is introduced at height z0, convective ERR Q0
 # is used for some properties, top-hat profiles for temperature and velocity
-# are changed to gaussian, T0 and u0 describe centerline properties, 
+# are changed to gaussian, T0 and u0 describe center line properties,
 # Boussinesq approximation removed so that large density differences can
 # be taken into account - strong plumes.
 # ERR: Energy release rate, in kW
@@ -142,56 +139,60 @@ def ZukoskiPlumeMassFlow(ERR,z,g=9.81,cp=1.0,T_infty=293,rho_infty=1.2):
 # Virtual origin z0
 # Formula 4.23 [1]
 # Based on fire source diameter relD and total energy released ERR
-def HeskestadVirtualOrigin(ERR,relD):
-    z0 = 0.083 *(ERR)**(2.0/5.0) - (1.02 * relD)
+def heskestad_virtual_origin(err, rel_diam):
+    z0 = 0.083 * err ** (2.0 / 5.0) - (1.02 * rel_diam)
     return z0
+
 
 # Convective energy release rate (for completeness)
 # Usually between 20 % to 40 % of the total energy are lost due to radiation,
 # the residue is responsible for convection.
-
-def ConvectiveERR(ERR, radiation = 0.3):
-    Qc = ERR * (1 - radiation)
+def convective_err(err, radiation=0.3):
+    Qc = err * (1 - radiation)
     return Qc
 
-### Properties below the mean flame height L ###
+
+# Properties below the mean flame height L ###
 
 # Mass flow of plume at height z (below flame)
 # Formula 4.27 [1].
-def HeskestadPlumeMassFlowBelow(Qc,z,L):
+def heskestad_plume_mass_flow_below(Qc, z, L):
     m = 0.0056 * Qc *(z / L)
     return m
     
 
-### Properties above the mean flame height L ###
+# Properties above the mean flame height L ###
 
 # Plume radius (above flame)
 # Formula 4.24 [1]
-def HeskestadPlumeRadiusAbove(T0, z0, z, T_infty=293,):
-    b = 0.12 * (T0/T_infty)**(0.5) * (z - z0)
+def heskestad_plume_radius_above(T0, z0, z, T_infty=293, ):
+    b = 0.12 * (T0/T_infty)**0.5 * (z - z0)
     return b
+
 
 # Temperature difference of the plume at height z (above flame)
 # Formula 4.25 [1].
-def HeskestadPlumeTemperatureAbove(Qc,z,z0,g=9.81,cp=1.0,
-                                   T_infty=293,rho_infty=1.2):
-    a = 9.1*((T_infty)/(g * cp**2 * rho_infty**2))**(1.0/3.0)
+def heskestad_plume_temperature_above(Qc, z, z0, g=9.81, cp=1.0,
+                                      T_infty=293, rho_infty=1.2):
+    a = 9.1*(T_infty/(g * cp**2 * rho_infty**2))**(1.0/3.0)
     T = a *Qc**(2.0/3.0)*(z-z0)**(-5.0/3.0)
     return T
 
+
 # Upward gas velocity of the plume at height z (above flame)
 # Formula 4.26 [1].
-def HeskestadPlumeVelocityAbove(Qc,z,z0,g=9.81,cp=1.0,
-                                T_infty=293,rho_infty=1.2):
-    a = 3.4*((g)/(cp * T_infty * rho_infty))**(1.0/3.0)
+def heskestad_plume_velocity_above(Qc, z, z0, g=9.81, cp=1.0,
+                                   T_infty=293, rho_infty=1.2):
+    a = 3.4*(g/(cp * T_infty * rho_infty))**(1.0/3.0)
     u = a *Qc**(1.0/3.0)*(z-z0)**(-1.0/3.0)
     return u
 
+
 # Mass flow of plume at height z (above flame)
 # Formula 4.27 [1].
-def HeskestadPlumeMassFlowAbove(Qc,z,z0,g=9.81,cp=1.0,
-                                T_infty=293,rho_infty=1.2):
-    a = 0.071 * Qc**(1.0/3.0) * (z - (z0))**(5.0/3.0)
+def heskestad_plume_mass_flow_above(Qc, z, z0, g=9.81, cp=1.0,
+                                    T_infty=293, rho_infty=1.2):
+    a = 0.071 * Qc**(1.0/3.0) * (z - z0)**(5.0/3.0)
     m = a + 1.92*10**(-3.0) * Qc
     return m
 
@@ -210,22 +211,22 @@ z1 = 2.5
 z2 = 6.0
 
 # Calculate virtual origin 
-z0 = HeskestadVirtualOrigin(ERR,relD)
+z0 = heskestad_virtual_origin(ERR,relD)
 
 # Mean flame height
 L = MeanFlameHeight(ERR,relD)
 
 # Convective ERR
-Qc = ConvectiveERR(ERR)
+Qc = convective_err(ERR)
 
 # Mass flow within flame
-m1 = HeskestadPlumeMassFlowBelow(Qc,z1,L)
+m1 = heskestad_plume_mass_flow_below(Qc,z1,L)
 
 # Mass flow above flame 
-m2 = HeskestadPlumeMassFlowAbove(Qc,z2,z0)
+m2 = heskestad_plume_mass_flow_above(Qc,z2,z0)
 
-# Centerline temperature above flame
-T = HeskestadPlumeTemperatureAbove(Qc,z2,z0)
+# Center line temperature above flame
+T = heskestad_plume_temperature_above(Qc,z2,z0)
 
 
 print 'Virtual origin: ' + str(z0) + ' m'
@@ -236,8 +237,6 @@ print 'Mass flow (6.0 m): ' + str(m2) + ' kg/s'
 print 'Temperature: ' + str(T+293) + ' K'
 '''
 
-
-#%%
 
 # McCaffrey plume equations
 # Description:
@@ -253,20 +252,22 @@ print 'Temperature: ' + str(T+293) + ' K'
 
 # Determine the plume region
 # Table 4.1 [1].
-def McCaffreyPlumeRegion(ERR,z):
-    region = z/ERR**(2.0/5.0)
+def mccaffrey_plume_region(ERR, z):
+    region = z / ERR ** (2.0 / 5.0)
     return region
+
 
 # Temperature difference of the plume at height z
 # Formula 4.29 [1].
-def McCaffreyPlumeTemperture(ERR,k,n,z,g=9.81,T_infty=293):
+def mccaffrey_plume_temperature(ERR, k, n, z, g=9.81, T_infty=293):
     a = (k/(0.9 * np.sqrt(2*g)))**2.0
-    T0 = a * (z/ERR**(2.0/5.0))**(2 * n - 1.0) * T_infty
+    T0 = a * (z / ERR ** (2.0 / 5.0)) ** (2 * n - 1.0) * T_infty
     return T0
+
 
 # Upward gas velocity of the plume at height z
 # Formula 4.30 [1].
-def McCaffreyPlumeVelocity(ERR,k,n,z):
+def mccaffrey_plume_velocity(ERR, k, n, z):
     u0 = k * (z/ERR**(2.0/5.0))**n * ERR**(1.0/5.0)
     return u0
 
@@ -293,16 +294,16 @@ n2 = -1.0/3.0
 
 
 # Determine region at 2.5 m
-r1 = McCaffreyPlumeRegion(ERR,z1)
+r1 = mccaffrey_plume_region(ERR,z1)
 
 # Determine region at 6.0 m
-r2 = McCaffreyPlumeRegion(ERR,z2)
+r2 = mccaffrey_plume_region(ERR,z2)
 
-# Centerline temperature at 2.5 m
-T1 = McCaffreyPlumeTemperture(ERR,k1,n1,z1)
+# Center line temperature at 2.5 m
+T1 = mccaffrey_plume_temperature(ERR,k1,n1,z1)
 
-# Centerline temperature at 6.0 m
-T2 = McCaffreyPlumeTemperture(ERR,k2,n2,z2)
+# Center line temperature at 6.0 m
+T2 = mccaffrey_plume_temperature(ERR,k2,n2,z2)
 
 
 print 'Region at 2.5 m: ' + str(r1)
@@ -310,45 +311,3 @@ print 'Region at 6.0 m: ' + str(r2)
 print 'Temperature at 2.5 m: ' + str(T1+20) + ' K'
 print 'Temperature at 6.0 m: ' + str(T2+20) + ' K'
 '''
-
-
-#%%
-
-
-
-#%%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
